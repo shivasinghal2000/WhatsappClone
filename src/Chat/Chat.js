7e4760295f03c7c71303479b5ba76a3a80647647
shivasinghal2000 <singhalshiva936@gmail.com>
2021-08-24 17:38:12 +0530

    basic ui completed

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import './Chat.css'
+import React from "react";
+import "./Chat.css";
 function Chat() {
-    return (
-        <div>
-
-            <h1>I am a Chat</h1>
-        </div>
-    )
+  return (
+    <div className="chat">
+      <h1>I am a Chat</h1>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import './Sidebar.css'
 function Sidebar() {
     return (
-        <div>
+        <div className="sidebar">
             <h1>I am a sidebar</h1>
         </div>
     )
